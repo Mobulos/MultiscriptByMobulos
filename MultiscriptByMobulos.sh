d23aa34bd92b5d390b9c84fa2cf6368e48fa6a81
Mobulos <fabian@schmeltzer.info>
2022-03-09 22:42:15 +0100

    Update 3.3.6
    Hinzugefügt:
     ---
    
    Verbessert:
     - Fehler Nachrichten
     - Internet check über GitHub Server
     - Apache Installation automatisiert
    
    Entfernt:
     ---

--- MultiscriptByMobulos.sh
+++ MultiscriptByMobulos.sh
@@ -10,8 +10,8 @@
 
 ############################################
 ################# CHANGE ###################
-ver=3.3.5
-dat=22.02.2022
+ver=3.3.6
+dat=09.03.2022
 filescript=MultiscriptByMobulos.sh
 link=https://raw.githubusercontent.com/Mobulos/MultiscriptByMobulos/master/MultiscriptByMobulos.sh
 ############################################
@@ -47,21 +47,21 @@ function error
 {
     clear
     echo -n "$red"
-    echo "FEHLER #$code"
+    echo "FEHLER: #$code"
     echo "$reset"
     echo "Bitte erstelle ein$yellow issue$reset auf Github: 'https://github.com/Mobulos/MultiscriptByMobulos/issues' und nenne den Fehlercode #$code"
     echo
-    echo "Wenn diese meldung nach einem erneuten start erscheint und du das Script weiterhin nutzen willst musst du follgende Befehle eingeben:"
+    echo "Wenn diese Meldung nach einem erneuten start erscheint und du das Script weiterhin nutzen willst musst du follgende Befehle eingeben:"
     echo "$red"
     echo "WARNUNG diese Befehle beenden alle screens und löschen alle Bots!!! Mache gegebenenfalls ein Backup der Bots!!!"
-    echo -n "$yellow"
+    echo  "$yellow"
     echo "'pkill screen || rm -r /home/bot* || rm ports user'"
-    echo "Ausserdem musst du die Bot User löschen, das machst du mit deluser [user]"
-    echo "Das [User] ersetzt du jetzt mit jeden der follgenden User:"
+    echo "Ausserdem musst du die Bot User löschen, das machst du mit:  deluser [user]"
+    echo "Das [user] ersetzt du jetzt mit jeden der follgenden User:"
     echo "Beispiel: Aus bot1:8087 wird 'deluser bot1'"
     cat user    
     echo "$reset"
-    echo "Dannach kannst du das Script wie gewohnt starten"
+    echo "Dannach kannst du das Script wie gewohnt starten."
     exit 0
 }
 
@@ -77,12 +77,13 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 #Internet check
-if ping -c 1 1.1.1.1; then
+if ping -c 1 raw.githubusercontent.com; then
     echo ""
 else
     clear
-    echo "Es konnte keine Netzwerk Verbindung zu '1.1.1.1' hergestellt werden."
-    echo "Das Script kann ohne Internet nicht ausgeführt werden."
+    echo "Es konnte keine Netzwerk Verbindung zu den GitHub Servern hergestellt werden."
+    echo "Das Script kann ohne diese Verbindung nicht ausgeführt werden."
+    echo "Bitte überprüfe deine Internet Verbingung."
     exit 1
 fi
 
@@ -124,12 +125,25 @@ install:
     do
         apt-get install -y $i
     done
+
+    #Apache2 install (check)
+    apt -qq list apache2 | grep -v "installed" | awk -F/ '{print $1}' > /root/list.txt
+    packages=$(cat /root/list.txt)
+    grep -q '[^[:space:]]' < /root/list.txt
+    CHECK_LIST=$?
+    if [ $CHECK_LIST -eq 1 ]; then
+        echo -n ""
+    else
+        apt-get install -y apache2
+    fi
+
+    clear
     jumpto $installall
 
 
 installall:
   clear
-  read -n1 -p "Hast du Ubuntu 18.04? (Y|N) Falls du dir nicht sicher bist probiere es mit nein '(N)' " ubuntu
+  read -n1 -p "Hast du Ubuntu 18.04? (Y|N) Falls du dir nicht sicher bist probiere es mit nein (N)" ubuntu
         case $ubuntu in
         Y|y|J|j)
             add-apt-repository universe
@@ -150,7 +164,6 @@ menue:
     clear
 
 failedmenue:
-    clear
     echo "$yellow########################################"
     read -t0.1
     echo "#####  SinusBot Script by Mobulos  #####"
@@ -163,32 +176,36 @@ failedmenue:
     echo "Version: $ver"
     read -t0.1
     echo "Update vom: $dat"
+
     tmp=($(tput setaf 2)) && echo "$tmp"
     read -t0.1
     echo "  1. Bot installieren"
+
     tmp=($(tput setaf 3)) && echo -n "$tmp"
     read -t0.1
     echo "  2. Bot löschen"
+
     tmp=($(tput setaf 4)) && echo -n "$tmp"
     read -t0.1
     echo "  3. Bot starten / stoppen"
+
     tmp=($(tput setaf 4)) && echo -n "$tmp"
     read -t0.1
     echo "  4. Scripts für den SinusBot installieren"
+
     tmp=($(tput setaf 5)) && echo -n "$tmp"
     read -t0.1
     echo "  5. Existierende nutzer anzeigen"
+
     tmp=($(tput setaf 6)) && echo -n "$tmp"
     read -t0.1
     echo "  6. Script Updaten"
-    read -t0.1
-    tmp=($(tput setaf 6))
-    echo -n "$tmp"
-    echo "  7. Apache2 install"
+
     read -t0.1
     tmp=($(tput setaf 1))
     echo -n "$tmp"
-    echo "  8. Exit"
+    echo "  7. Exit"
+
     echo "$reset"
     read -n1 -p "Bot Befehle: " befehl
     case $befehl in
@@ -203,7 +220,7 @@ failedmenue:
     3)
         clear
         echo "Leider können die Bots noch nicht mit dem Script gestartet oder gestoppt werden."
-        echo "Dies musst du manuell machen, doch all zu schwer ist das nicht."
+        echo "Dies muss manuell gemacht werden, doch all zu schwer ist das nicht."
         echo "Logge dich zuerst in den Bot ein mit: (ersetze bot1 durch deinen Bot)"
         echo "$yellow"
         echo "su - bot1"
@@ -211,6 +228,7 @@ failedmenue:
         echo -n "$reset"
         echo "Oder zum stoppen:"
         echo -n "$yellow"
+        echo "su - bot1"
         echo "./stop.sh"
         echo "$reset"
         exit 0
@@ -223,11 +241,9 @@ failedmenue:
     5)
         clear
         echo "Folgende Benutzer exsistieren bereits:"
-        echo
         cat user
         echo
         echo "Follgende Ports sind eingetragen:"
-        echo
         cat ports
         echo
         read -n1 -p "Drücke eine Taste, um fortzufahren."
@@ -242,33 +258,16 @@ failedmenue:
         wget $link
         chmod +x $filescript
         clear
-        echo "Update abgeschlossen, du kannst das Script jetzt erneut starten."
+        echo "Update abgeschlossen, das Script kann jetzt erneut gestartet werden."
         exit 0
   	;;
-    7)
-        clear
-        apt -qq list apache2 | grep -v "installed" | awk -F/ '{print $1}' > /root/list.txt
-        packages=$(cat /root/list.txt)
-        grep -q '[^[:space:]]' < /root/list.txt
-        CHECK_LIST=$?
-        if [ $CHECK_LIST -eq 1 ]; then
-            clear
-            echo $red"Apache2 ist bereits installiert."
-            read -n1 -t3
-        else
-            apt-get install -y apache2
-            clear
-        fi
-        read -n2 -t3 "$green Apache2 wurde installiert $reset"
-        jumpto $menue
-    ;;
-  	8|q|Q|E|e)
+  	7|q|Q|E|e)
         clear
         exit 0
   	;;
   	*)
         clear
-        echo "Eingabe wird nicht Akzeptiert."
+        echo "Die Eingabe wird nicht Akzeptiert."
         read -t3 -n1
         jumpto $failedmenue
   	;;
@@ -301,14 +300,14 @@ start:
     adduser $name sudo
     sh -c "echo '$name ALL=NOPASSWD: ALL' >> /etc/sudoers"
     clear
-    echo "Erforderliche Daten werden herruntergeladen"
+    echo "Erforderliche Daten werden herruntergeladen."
     wget -P /home/$name/ 'https://raw.githubusercontent.com/Mobulos/MultiscriptByMobulos/download/sinusbot.current.zip'
     sudo chown $name /var/run/screen/S-$name
     # TS-CLient
     wget -P /home/$name/ 'https://files.teamspeak-services.com/releases/client/3.5.3/TeamSpeak3-Client-linux_amd64-3.5.3.run'
     clear
     echo "$red"
-    echo  "Bitte beachte, dass das Passwort local als ROH-Datei gespeichert wird."
+    echo  "Bitte beachte, dass das Passwort Lokal als ROH-Datei gespeichert wird."
     echo "$reset"
     read -p "Bitte erstelle ein Passwort fuer den Sinusbot: $reset" pw
     clear
@@ -330,6 +329,7 @@ start:
             break
         fi
     done
+
     #1ststart.sh schreiben
     echo "sudo unzip /home/$name/sinusbot.current.zip" >> /home/$name/1ststart.sh
     echo "sudo rm /home/$name/sinusbot.current.zip" >> /home/$name/1ststart.sh
@@ -349,7 +349,7 @@ start:
     echo "echo 'screen -dmS $name ./sinusbot --override-password=$pw' >> /home/$name/start.sh" >> /home/$name/1ststart.sh
     echo "sudo chmod u+x /home/$name/start.sh" >> /home/$name/1ststart.sh
     echo "clear" >> /home/$name/1ststart.sh
-    echo "echo Du kannst den Bot ab jetzt mit dem Befehl './start.sh' starten" >> /home/$name/1ststart.sh
+    echo "echo Der Bot kann jetzt mit den Befehl './start.sh' gestartet werden" >> /home/$name/1ststart.sh
     echo "echo" >> /home/$name/1ststart.sh
     echo "echo Login Daten:" >> /home/$name/1ststart.sh
     echo "curl ifconfig.me;echo :$port" >> /home/$name/1ststart.sh
@@ -373,7 +373,7 @@ start:
     echo "sudo chown -R $name:$name /home/$name/*" >> /home/$name/1ststart.sh
     echo "rm /home/$name/1ststart.sh" >> /home/$name/1ststart.sh
     chmod u+x /home/$name/1ststart.sh
-    pw=???
+    pw="???"
 
     #User Datei Eintrag
     echo "$name:$port" >> user
@@ -394,6 +394,7 @@ start:
     cd /home/$name
     su $name -c /home/$name/1ststart.sh
 
+    #YouTube DL
     echo -n "$yellow"
     echo "Youtube Downloader installieren."
     echo -n "$reset"
@@ -414,7 +415,7 @@ installscripts:
     done
     echo
     echo
-    read -n1 -p "Willst du diese Scripts installieren? (Y|N) " scripts
+    read -n1 -p "Sollen diese Scripts installiert werden? (Y|N) " scripts
     case $scripts in
     Y|y|J|j)
 		clear
@@ -436,7 +437,7 @@ installscripts:
         echo "clear" >> /home/$name/scriptinstall.sh
         sudo chmod 777 /home/$name/scriptinstall.sh
         clear
-        echo "Ich kann zusätzlich noch Scripts löschen, die in den meisten Fällen nicht benötigt werden."
+        echo "Es können zusätzlich noch Scripts gelöscht werden, welche in den meisten Fällen nicht benötigt werden."
         echo "Diese Scripts würden gelöscht werden:"
         echo
         for i in advertising alonemode bookmark followme norecording rememberChannel welcome
@@ -448,7 +449,7 @@ installscripts:
         echo
         echo
         echo "Die Script können im nachhinein erneut installiert werden."
-        read -n1 -p "Soll ich sie  LÖSCHEN? (Y|N)" scriptsdel
+        read -n1 -p "Soll sie GELÖSCHT werden? (Y|N)" scriptsdel
         case $scriptsdel in
             Y|y|J|j)
             clear
@@ -487,12 +488,12 @@ delete:
     echo "Mögliche Eigabe: bot1"
     echo "Falsche Eingabe bot1:8087"
     echo
-    read -p "Welchen Bot möchtest du löschen? " name
+    read -p "Welcher Bot soll gelöscht werden? " name
     killall -u $name
     pkill $name
     clear
     cat /home/$name/port
-    read -p "bitte gebe zur verifizierung die obenstehenden Zahlen ein: " ports
+    read -p "Bitte gebe zur Verifizierung die obenstehenden Zahlen ein: " ports
     grep -v "$name:$ports" user > user2
     mv user2 user
     grep -v "$ports" ports > ports2
